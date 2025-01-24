aed9e7f8375231e47d1aeb3e20992e363863802b
Timothy Zatloff <timzatloff@gmail.com>
2025-01-23 22:08:25 -0500

    'Trhird commit is ready'

--- Git.py001.py
+++ Git.py001.py
@@ -25,3 +25,10 @@ if __name__ == "__main__":
     print(greet(user_name))
     number = int(input("Enter a number to calculate its factorial: "))
     print(f"The factorial of {number} is {factorial(number)}")
+def greet(name): return f"Hello, {name}!"
+
+def factorial(n): return 1 if n == 0 else n * factorial(n-1)
+
+if __name__ == "__main__":
+    print(greet(input("Enter your name: ")))
+    print(f"The factorial of {number := int(input('Enter a number to calculate its factorial: '))} is {factorial(number)}")
